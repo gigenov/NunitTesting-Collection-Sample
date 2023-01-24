48c5f033ec2e801b7ab89fbe3aa04ccf0d3c00cd
Vangel Tuparov <37338316+viitvangel@users.noreply.github.com>
2023-01-25 01:12:53 +0200

    4/5 NUnit tests done

--- Collection.UnitTests/CollectionTests.cs
+++ Collection.UnitTests/CollectionTests.cs
@@ -14,5 +14,62 @@ namespace Collection.UnitTests
             //Assert
             Assert.AreEqual(coll.ToString(), "[]");
         }
+
+        [Test]
+        public void Test_Collection_RemoveAtMiddle()
+        {
+            // Arrange
+            var coll = new Collection<int>(3, 5, 6);
+
+            // Act
+            coll.RemoveAt(1);
+
+            // Assert
+            Assert.AreEqual(coll.ToString(), "[3, 6]");
+
+        }
+
+        [Test]
+        public void Test_Collection_RemoveAtInvalidIndex()
+        {
+            // Arrange
+            var coll = new Collection<int>(3, 5, 6);
+
+
+            // Act and Assert
+            Assert.That(() => { coll.RemoveAt(3); }, Throws.InstanceOf<ArgumentOutOfRangeException>()); 
+
+        }
+
+        [Test]
+        public void Test_Collection_RemoveAll()
+        {
+           
+
+        }
+
+        [Test]
+        public void Test_Collection_Clear()
+        {
+            // Arrange
+            var coll = new Collection<int>(3, 5, 6);
+
+            // Act 
+            coll.Clear();
+
+            //Assert
+            Assert.AreEqual(0, coll.Count);
+        }
+
+        [Test]
+        public void Test_Collection_CountAndCapacity()
+        {
+            // Arrange
+            var coll = new Collection<int>(3, 5, 6);
+
+            //Assert
+            Assert.AreEqual(3, coll.Count);
+            Assert.That(coll.Capacity, Is.GreaterThanOrEqualTo(coll.Count) );
+        }
     }
 }
