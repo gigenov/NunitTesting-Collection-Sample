4450daa5b9826a0710037da975b1c7826654d462
Радостина Анастасова <ina_rd@abv.bg>
2023-01-25 12:10:10 +0200

    added insert unit tests branch – initial commit

--- Collection.UnitTests/CollectionTests.cs
+++ Collection.UnitTests/CollectionTests.cs
@@ -14,5 +14,37 @@ namespace Collection.UnitTests
             //Assert
             Assert.AreEqual(coll.ToString(), "[]");
         }
+
+        // Insert tests Radostina
+        [Test]
+        public void Test_Collection_InsertAtStart()
+        {
+            Console.WriteLine("TO DO Test_Collection_InsertAtStart");
+        }
+
+        [Test]
+        public void Test_Collection_InsertAtEnd()
+        {
+            Console.WriteLine("Test_Collection_InsertAtEnd");
+        }
+
+        [Test]
+        public void Test_Collection_InsertAtMiddle()
+        {
+            Console.WriteLine("TO DO Test_Collection_InsertAtMiddle");
+        }
+
+        [Test]
+        public void Test_Collection_InsertAtWithGrow()
+        {
+            Console.WriteLine("TO DO Test_Collection_InsertAtWithGrow");
+        }
+
+        [Test]
+        public void Test_Collection_InsertAtInvalidIndex()
+        {
+            Console.WriteLine("TO DO Test_Collection_InsertAtInvalidIndex");
+        }
+
     }
 }
