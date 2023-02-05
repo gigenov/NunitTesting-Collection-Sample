4bbcd029ddeb2367ce757a4299ccebcedaeb19e4
Georgi Genov <georgi.genov89@yahoo.com>
2023-02-05 20:51:11 +0200

    adding new unit tests

--- Collection.UnitTests/CollectionTests.cs
+++ Collection.UnitTests/CollectionTests.cs
@@ -46,5 +46,67 @@ namespace Collection.UnitTests
             //Assert
             Assert.That(coll.ToString(), Is.EqualTo("[2, 3, 4]"));
         }
+
+        [Test]
+        public void Test_Collection_AddRaange()
+        {
+            //Arrange and Act
+            var coll = new Collection<int>(2, 3, 4);
+
+            coll.AddRange(new[] { 5, 6, 7 });    
+
+            //Assert
+            Assert.That(coll.ToString(), Is.EqualTo("[2, 3, 4, 5, 6, 7]"));
+        }
+
+        [Test]
+
+        public void Test_Collection_AddRangeWithGrow()
+
+        {
+
+            var nums = new Collection<int>();
+
+            int oldCapacity = nums.Capacity;
+
+            var newNums = Enumerable.Range(1000, 2000).ToArray();
+
+            nums.AddRange(newNums);
+
+            string expectedNums = "[" + string.Join(", ", newNums) + "]";
+
+            Assert.That(nums.ToString(), Is.EqualTo(expectedNums));
+
+            Assert.That(nums.Capacity, Is.GreaterThanOrEqualTo(oldCapacity));
+
+            Assert.That(nums.Capacity, Is.GreaterThanOrEqualTo(nums.Count));
+
+        }
+
+        [Test]
+
+        public void Test_Collection_ToStringNestedCollections()
+        {
+            var names = new Collection<string>("Teddy", "Gerry");
+            var nums = new Collection<int>(10, 20);
+            var dates = new Collection<DateTime>();
+            var nested = new Collection<object>(names, nums, dates);
+            string nestedToString = nested.ToString();
+
+            Assert.That(nestedToString,
+              Is.EqualTo("[[Teddy, Gerry], [10, 20], []]"));
+        }
+
+        [TestCase ("Peter,Maria,Ivan", 0,"Peter")]
+        [TestCase ("Peter,Maria,Ivan", 01,"Maria")]
+        [TestCase ("Peter,Maria,Ivan,,,", 4,"")]
+        [TestCase ("Peter,Maria,Ivan,Koko", 3,"Koko")]
+        public void Test_Collection_GetByValidIndix(string data, int index, string expected)
+        {
+            var coll = new Collection<string>(data.Split(","));
+
+            var actual = coll[index];
+            Assert.That(actual, Is.EqualTo(expected));
+        }
     }
 }
